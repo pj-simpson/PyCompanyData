7f046deca116a4d9e4afadca16cd1d023699261f
Peter Simpson <p.simpson@codat.io>
2022-12-12 13:49:13 +0000

    pre-commit now all working?

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.10", "3.9", "3.8", "3.7"])
+@nox.session(python=["3.11", "3.10", "3.9", "3.8", "3.7"])
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest", ".")
