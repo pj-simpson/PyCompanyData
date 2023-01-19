17b812ab10e5692eac60a49ba0c38cf0d9119f72
Tim Hoare <t.hoare@codat.io>
2023-01-19 09:41:39 +0000

    Merged PR 56420: added back invoices
    added back invoices

--- src/pycodat/clients/accounting_client.py
+++ src/pycodat/clients/accounting_client.py
@@ -245,3 +245,61 @@ class AccountingClient(BaseClient):
         """
         suppliers_handler = SuppliersHandler(self.key, self.env)
         return suppliers_handler.get_single_supplier(company_id, supplier_id)
+
+    def get_invoices(self, company_id: str, query: str = None, order_by: str = None) -> typing.List[Invoice]:
+        """Gets all invoices for a company
+
+        :param company_id: Unique identifier for a company
+        :type company_id: str
+
+        :param query: Query to pass to Codat API to filter results
+        :type query: str
+
+        :param order_by: Name of the field to order the results by.  Defaults to ascending; prefix with `-` to sort in descending order.
+        :type orderby: str
+
+        :return: A list of invoices
+        :rtype: typing.List[Invoice]
+        """
+        invoice_handler = InvoicesHandler(self.key, self.env)
+        return invoice_handler.get_all_invoices(company_id, query, order_by)
+
+    def get_invoices_page(self, company_id: str, page_number: int = 1, page_size: int = 100, query: str = None,
+                          order_by: str = None) -> PaginatedResponse[Invoice]:
+        """Gets a page of invoices for a company
+
+        :param company_id: Unique identifier for a company
+        :type company_id: str
+
+        :param query: Query to pass to Codat API to filter results
+        :type query: str
+
+        :param order_by: Name of the field to order the results by.  Defaults to ascending; prefix with `-` to sort in descending order.
+        :type orderby: str
+
+        :param page_number: Page number to retrieve.  Default: 1
+        :type page_number: int
+
+        :param page_size: Number of records to retrieve.  Default: 100
+        :type page_size: int
+
+        :return: A page of invoices
+        :rtype: PaginatedResponse[Invoice]
+        """
+        invoice_handler = InvoicesHandler(self.key, self.env)
+        return invoice_handler.get_pageof_invoices(company_id, page_number, page_size, query, order_by)
+
+    def get_invoice(self, company_id: str, invoice_id: str) -> Invoice:
+        """Gets an invoice (by ID) for a company
+
+        :param company_id: Unique identifier for a company
+        :type company_id: str
+
+        :param invoice_id: Unique identifier for the invoice
+        :type invoice_id: str
+
+        :return: An invoice
+        :rtype: Invoice
+        """
+        invoice_handler = InvoicesHandler(self.key, self.env)
+        return invoice_handler.get_single_invoice(company_id, invoice_id)
