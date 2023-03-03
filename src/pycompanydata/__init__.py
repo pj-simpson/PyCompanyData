d151d62eca45907432526597429c7df6f3804d37
Peter Simpson <p.simpson@codat.io>
2023-03-03 14:14:06 +0000

    incrementing version

--- src/pycompanydata/__init__.py
+++ src/pycompanydata/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from pycompanydata.clients.accounting_client import AccountingClient
 from pycompanydata.clients.platform_client import PlatformClient
